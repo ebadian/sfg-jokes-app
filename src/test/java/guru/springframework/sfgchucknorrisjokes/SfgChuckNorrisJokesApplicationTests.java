d6e3301c6f2d4cb063047afe7ebbfa9b61d84c80
ebadiana <a.ebadian@elsevier.com>
2020-11-20 12:20:02 +0000

    Creating a simple spring project from https://start.spring.io/ and adding the chuck-norris-for-actuator dependency.

---
+++ src/test/java/guru/springframework/sfgchucknorrisjokes/SfgChuckNorrisJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgchucknorrisjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgChuckNorrisJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
