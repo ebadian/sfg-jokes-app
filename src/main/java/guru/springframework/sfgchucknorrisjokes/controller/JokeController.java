f5d000f8e8bc49adbf856b9067da3ef0904f6c08
ebadiana <a.ebadian@elsevier.com>
2020-11-22 09:48:21 +0000

    Using Java configuration Injecting ChuckNorrisQuotes as a service with Java configurations.    - In JokeController, JokeService now is injected via constructor.    - Created a new package for Configs, created a ChuckConfiguration class in it    - Annotated the ChuckConfiguration with @Configuration    - Defined ChuckNorrisQuotes as beans in ChuckConfiguration class.

--- src/main/java/guru/springframework/sfgchucknorrisjokes/controller/JokeController.java
+++ src/main/java/guru/springframework/sfgchucknorrisjokes/controller/JokeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokeController {
+
     private JokesService jokesService;
 
     public JokeController(JokesService jokesService) {
